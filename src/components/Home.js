bb488929fb858bf91f80b5c1e9bde74403c51ae7
winaung1 <91712429+winaung1@users.noreply.github.com>
2022-08-03 20:09:01 -0600

    it going good

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ import { Topsection } from './Topsection';
 import { Bodysection } from './Bodysection';
 
 export const Home = (props) => {
-  const [appear, setAppear] = useState(false);
+
   return (
-    <div onClick={() => setAppear(!appear)} className="home relative flex justify-center items-center text-white font-medium  text-sm  py-2.5 text-center mb-2 min-h-screen w-full">
+    <div className="home relative flex justify-center items-center text-white font-medium  text-sm  py-2.5 text-center mb-2 min-h-screen w-full">
         <div className='text-gray-500'>
-          {appear && <div>
+         <div>
             <Topsection/>
             <Bodysection />
-          </div>}
+          </div>
         </div>
     </div>
   )
