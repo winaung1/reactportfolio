330e53b69bccaf36ec1a65f07b668df6265b680b
winaung1 <91712429+winaung1@users.noreply.github.com>
2022-08-05 22:35:36 -0600

    done with what I do screens

---
+++ src/components/Lines.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Lines = () => {
+  return (
+    <div className='mx-auto'>
+        <div className="hidden md:block lg:w-[800px] h-[1px] w-[600px] bg-gray-400"></div>
+        <div className="hidden md:block rotate-[90deg] h-[.5px] w-[600px] lg:w-[800px] bg-gray-400"></div>
+    </div>
+  )
+}
